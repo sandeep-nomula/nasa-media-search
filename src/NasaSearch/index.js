b96a70cda79b956748ab6b29d2f6cc9d9a4b079c
Sandeep Kumar Nomula <sandykumar.nomula@gmail.com>
2021-03-26 12:45:10 +0000

    indentation

--- src/NasaSearch/index.js
+++ src/NasaSearch/index.js
@@ -24,9 +24,9 @@ padding-top:${props => props.paddingTop}
 
 const url = "https://images-api.nasa.gov/search"
 const options = [
-    { value: 'audio', label: 'Audio' },
-    { value: 'video', label: 'Video' },
-    { value: 'image', label: 'Image' },
+  { value: 'audio', label: 'Audio' },
+  { value: 'video', label: 'Video' },
+  { value: 'image', label: 'Image' },
 ];
 
 const date = new Date()
@@ -34,132 +34,133 @@ const maxYear = date.getFullYear();
 
 export const NasaSearch = () => {
 
-    const [data, setData] = useState([]);
-    const [isSubmitting, setIsSubmitting] = useState(false);
+  const [data, setData] = useState([]);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-    const formSchema = yup.object().shape({
-        keywords: yup.string()
-            .required("Please enter keywords to search.")
-            .min(2, "Keywords must be between 2 and 50 characters.")
-            .max(20, "Keywords must be between 2 and 50 characters."),
+  const formSchema = yup.object().shape({
+    keywords: yup.string()
+      .required("Please enter keywords to search.")
+      .min(2, "Keywords must be between 2 and 50 characters.")
+      .max(20, "Keywords must be between 2 and 50 characters."),
 
-        mediaType: yup.string()
-            .required("Please select a media type."),
+    mediaType: yup.string()
+      .required("Please select a media type."),
 
-        yearStart: yup.number()
-            .positive("Please enter a valid year.")
-            .integer("Please enter a valid year.")
-            .max(maxYear, "Year must not be in the future.")
-    });
+    yearStart: yup.number()
+      .positive("Please enter a valid year.")
+      .integer("Please enter a valid year.")
+      .max(maxYear, "Year must not be in the future.")
+  });
 
-    const searchNasa = (values) => {
-        setIsSubmitting(true);
-        const { keywords, mediaType, yearStart } = values;
-        const params = {
-            q: keywords,
-            media_type: mediaType,
-            page: 1
-        };
-        if (yearStart) {
-            params.year_start = yearStart;
-        }
-        axios.get(url, {
-            params
-        })
-            .then(data => {
-                setData(data?.data?.collection?.items);
-                setIsSubmitting(false);
-            }).error(e => {
-                setIsSubmitting(false);
-            })
+  const searchNasa = (values) => {
+    setIsSubmitting(true);
+    const { keywords, mediaType, yearStart } = values;
+    const params = {
+      q: keywords,
+      media_type: mediaType,
+      page: 1
+    };
+    if (yearStart) {
+      params.year_start = yearStart;
     }
+    axios.get(url, {
+      params
+    })
+      .then(data => {
+        setData(data?.data?.collection?.items);
+        setIsSubmitting(false);
+      }).catch(e => {
+        console.log(e);
+        setIsSubmitting(false);
+      })
+  }
 
-    const getError = (errors, touched, key) => {
-        if (errors[key] && touched[key]) {
-            return errors[key];
-        }
-        return "";
+  const getError = (errors, touched, key) => {
+    if (errors[key] && touched[key]) {
+      return errors[key];
     }
-    return (
-        <><div>
-            <h1>CRUK technical exercise - React</h1>
-        </div>
-            <div>
-                <Formik
-                    validateOnChange
-                    valiidateOnSubmit
-                    initialValues={{
-                        keywords: "",
-                        mediaType: "",
-                        yearStart: ""
-                    }}
-                    validationSchema={formSchema}
-                    onSubmit={(values) => {
-                        searchNasa(values)
-                    }}>
-                    {({ errors, touched }) => {
-                        return (
-                            <Form>
-                                <Row>
-                                    <Column>
-                                        <Field name="keywords">
-                                            {({ field }) => (
-                                                <>
-                                                    <TextField
-                                                        label="Keywords"
-                                                        type="text"
-                                                        required
-                                                        {...field}
-                                                        errorMessage={getError(errors, touched, "keywords")}
-                                                    />
-                                                </>
-                                            )}
-                                        </Field>
-                                    </Column>
-                                    <Column>
-                                        <Field name="mediaType">
-                                            {({ field }) => (
-                                                <>
-                                                    <Select
-                                                        label="Media Type"
-                                                        type="text"
-                                                        required
-                                                        {...field}
-                                                        errorMessage={getError(errors, touched, "mediaType")}
-                                                    >
-                                                        <option disabled value="">--Please choose an option--</option>
-                                                        {options?.map(option => {
-                                                            return <option value={option.value} key={option.value}>{option.label}</option>
-                                                        })}
-                                                    </Select>
-                                                </>
-                                            )}
-                                        </Field>
-                                    </Column>
-                                    <Column>
-                                        <Field name="yearStart">
-                                            {({ field }) => (
-                                                <>
-                                                    <TextField
-                                                        label="Year Start"
-                                                        type="number"
-                                                        {...field}
-                                                        errorMessage={getError(errors, touched, "yearStart")}
-                                                    />
-                                                </>
-                                            )}
-                                        </Field>
-                                    </Column>
-                                    <Column paddingTop={"35px"}>
-                                        <Button disabled={isSubmitting} type="submit">{isSubmitting ? "Submitting.." : "Submit"}</Button>
-                                    </Column>
-                                </Row>
-                            </Form>
-                        )
-                    }}
-                </Formik>
-            </div>
-            <Divider />
-            {isSubmitting? <Loader/> : <NasaResults data={data} />}
-        </>)
+    return "";
+  }
+  return (
+    <><div>
+      <h1>CRUK technical exercise - React</h1>
+    </div>
+      <div>
+        <Formik
+          validateOnChange
+          valiidateOnSubmit
+          initialValues={{
+            keywords: "",
+            mediaType: "",
+            yearStart: ""
+          }}
+          validationSchema={formSchema}
+          onSubmit={(values) => {
+            searchNasa(values)
+          }}>
+          {({ errors, touched }) => {
+            return (
+              <Form>
+                <Row>
+                  <Column>
+                    <Field name="keywords">
+                      {({ field }) => (
+                        <>
+                          <TextField
+                            label="Keywords"
+                            type="text"
+                            required
+                            {...field}
+                            errorMessage={getError(errors, touched, "keywords")}
+                          />
+                        </>
+                      )}
+                    </Field>
+                  </Column>
+                  <Column>
+                    <Field name="mediaType">
+                      {({ field }) => (
+                        <>
+                          <Select
+                            label="Media Type"
+                            type="text"
+                            required
+                            {...field}
+                            errorMessage={getError(errors, touched, "mediaType")}
+                          >
+                            <option disabled value="">--Please choose an option--</option>
+                            {options?.map(option => {
+                              return <option value={option.value} key={option.value}>{option.label}</option>
+                            })}
+                          </Select>
+                        </>
+                      )}
+                    </Field>
+                  </Column>
+                  <Column>
+                    <Field name="yearStart">
+                      {({ field }) => (
+                        <>
+                          <TextField
+                            label="Year Start"
+                            type="number"
+                            {...field}
+                            errorMessage={getError(errors, touched, "yearStart")}
+                          />
+                        </>
+                      )}
+                    </Field>
+                  </Column>
+                  <Column paddingTop={"35px"}>
+                    <Button disabled={isSubmitting} type="submit">{isSubmitting ? "Submitting.." : "Submit"}</Button>
+                  </Column>
+                </Row>
+              </Form>
+            )
+          }}
+        </Formik>
+      </div>
+      <Divider />
+      {isSubmitting ? <Loader /> : <NasaResults data={data} />}
+    </>)
 }
